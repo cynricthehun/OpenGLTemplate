08146e154e8d9b64b8a22806af6f0417018b54b2
CynricTheHun <joeymeyer1986@gmail.com>
2019-01-13 22:40:44 -0600

    broke shaders out into individual files

--- OpenGLPlayground/OpenGLPlayground.cpp
+++ OpenGLPlayground/OpenGLPlayground.cpp
@@ -9,20 +9,6 @@
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-const GLchar *vertexShaderSource = "#version 330 core\n"
-"layout ( location = 0 ) in vec3 position;\n" 
-"void main( )\n"
-"{\n"
-"gl_Position = vec4( position.x, position.y, position.z, 1.0 );\n"
-"}\n";
-
-const GLchar *fragmentShaderSource = "#version 330 core\n"
-"out vec4 color;\n"
-"void main( )\n"
-"{\n"
-"color = vec4( 1.0f, 0.5f, 0.2f, 1.0f );\n"
-"}\n";
-
 int main()
 {
 	glfwInit();
